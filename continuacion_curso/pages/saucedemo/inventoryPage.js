1f31d8d1403e6facca035e9f8595708ac37e564d
FranciscoJavierJim <225529106+FranciscoJavierJim@users.noreply.github.com>
2025-09-10 08:38:31 +0200

    Eliminar carpeta

--- continuacion_curso/pages/saucedemo/inventoryPage.js
+++
@@ -1,8 +0,0 @@
-class inventoryPage{ // declara una clase llamada inventoryPage. Representa la pagina de inventario (justo despues de hacer el login)
-    elements = { // // crea una propiedad de clase llamada 'elements'. Aqui agruparemos funciones que devuelven localizadores de cypress
-        titleSpan: () => cy.get('.title') // definimos titleSpan, una funcion que ejecuta 'cy.get...' Es decir, busca el elemento que tenga la clase CSS .title (el encabezado que dice 'products' en saucedemo)
-    }
-
-}
-
-module.exports = new inventoryPage();
